1df0db946ba737b4d6e84fe73a485a8ca5c9139f
Diogo Correia <me@diogotc.com>
2020-09-07 10:20:51 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,81 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+const robotjs = require("robotjs");
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require("electron-squirrel-startup")) {
+  // eslint-disable-line global-require
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    width: 800,
+    height: 600,
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
+};
+
+const sleep = async (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on("ready", createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+let cancelPaste = false;
+
+ipcMain.on("handle-paste-cancel", () => {
+  cancelPaste = true;
+});
+
+ipcMain.on("handle-paste", (event, arg) => {
+  setTimeout(async () => {
+    const data = arg;
+    robotjs.setKeyboardDelay(1);
+    for (const isbn of Object.keys(data)) {
+      if (cancelPaste) {
+        cancelPaste = false;
+        break;
+      }
+      robotjs.typeStringDelayed(isbn, 6000);
+      robotjs.keyTap("tab");
+      await sleep(100);
+      robotjs.keyTap("tab");
+      robotjs.keyTap("tab");
+      robotjs.typeStringDelayed(data[isbn], 6000);
+      robotjs.keyTap("tab");
+      robotjs.keyTap("tab");
+      robotjs.keyTap("tab");
+      robotjs.keyTap("tab");
+      await sleep(250);
+    }
+    event.sender.send("paste-finished");
+  });
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
---
+++ src/main.js
@@ -0,0 +1,114 @@
+/* xlsx.js (C) 2013-present SheetJS -- https://sheetjs.com */
+const XLSX = require("xlsx");
+const { ipcRenderer } = require("electron");
+const axios = require("axios");
+
+let content = {};
+
+const WOOK_REGEX = /<script type="application\/ld\+json">[^]*?({[^]+})[^]*?<\/script>[^]*?<!-- Fim Google/;
+
+const processWb = function (wb) {
+  const XPORT = document.getElementById("exportBtn");
+
+  const worksheet = wb.Sheets[wb.SheetNames[0]];
+  const jsonData = XLSX.utils.sheet_to_json(worksheet);
+
+  const isbns = jsonData
+    .filter((v) => v.ISBN)
+    .map((v) => v.ISBN.replace(/-/g, ""));
+  const occurrences = {};
+  for (var i = 0; i < isbns.length; i++)
+    occurrences[isbns[i]] = (occurrences[isbns[i]] || 0) + 1;
+  content = occurrences;
+
+  XPORT.disabled = false;
+};
+
+const readFile = function (files) {
+  const f = files[0];
+  const reader = new FileReader();
+  reader.onload = function (e) {
+    let data = e.target.result;
+    data = new Uint8Array(data);
+    processWb(XLSX.read(data, { type: "array" }));
+  };
+  reader.readAsArrayBuffer(f);
+};
+
+const sleep = async (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const exportXlsx = () => startCountdown(content);
+
+const cancelExport = () => {
+  ipcRenderer.send("handle-paste-cancel");
+};
+
+ipcRenderer.on("paste-finished", () => {
+  cancelExportBtn.style.display = "none";
+});
+
+const startCountdown = async (args) => {
+  const COUNTDOWN = document.getElementById("countdown");
+  for (let i = 10; i > 0; i--) {
+    COUNTDOWN.innerText = i;
+    await sleep(1000);
+  }
+  COUNTDOWN.innerText = "";
+  ipcRenderer.send("handle-paste", args);
+  cancelExportBtn.style.display = "block";
+};
+
+const handleMiddleExport = (isbn) => () => {
+  const keys = Object.keys(content);
+  const index = keys.indexOf(isbn);
+  const filteredKeys = keys.slice(index);
+  const args = {};
+  filteredKeys.forEach((key) => (args[key] = content[key]));
+  startCountdown(args);
+
+  document.getElementById("countdown").scrollIntoView();
+};
+
+const fetchData = async function () {
+  const tableBody = document.getElementById("dataTableBody");
+  tableBody.innerHTML = "";
+
+  for (const isbn of Object.keys(content)) {
+    const qnt = content[isbn];
+    const response = await axios.get(`https://www.wook.pt/pesquisa/${isbn}`);
+    const dataString = WOOK_REGEX.exec(response.data)?.[1] || "{}";
+    const bookMetadata = JSON.parse(dataString);
+    tableBody.insertAdjacentHTML(
+      "beforeend",
+      `<tr>
+    <td>${bookMetadata.name}</td>
+    <td>${isbn}</td>
+    <td>${qnt}</td>
+    <td>${
+      (bookMetadata.offers && bookMetadata.offers.price) || "--"
+    }&nbsp;€</td>
+    <td><button id="start-middle-${isbn}">Começar daqui</button></td>
+    </tr>`
+    );
+    document
+      .getElementById(`start-middle-${isbn}`)
+      .addEventListener("click", handleMiddleExport(isbn), false);
+  }
+};
+
+// add event listeners
+const readIn = document.getElementById("readIn");
+const exportBtn = document.getElementById("exportBtn");
+const fetchDataBtn = document.getElementById("fetchDataBtn");
+const cancelExportBtn = document.getElementById("cancelExportBtn");
+
+readIn.addEventListener(
+  "change",
+  (e) => {
+    readFile(e.target.files);
+  },
+  false
+);
+exportBtn.addEventListener("click", exportXlsx, false);
+fetchDataBtn.addEventListener("click", fetchData, false);
+cancelExportBtn.addEventListener("click", cancelExport, false);
